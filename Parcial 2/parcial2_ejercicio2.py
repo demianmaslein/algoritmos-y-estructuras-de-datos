3e8bb1b9b70b99bc54e285e829d0eb03b2522ccf
demianmaslein <demianmasleinn@gmail.com>
2025-11-17 21:28:15 -0300

    Update parcial2_ejercicio2.py

--- Parcial 2/parcial2_ejercicio2.py
+++ Parcial 2/parcial2_ejercicio2.py
@@ -1,21 +1,6 @@
 from class_graph import Graph
 from class_tree import BinaryTree
-
-
-personajes = [
-    {"nombre": "Luke Skywalker", "episodios": [4, 5, 6, 8, 9]},
-    {"nombre": "Darth Vader", "episodios": [3, 4, 5, 6]},
-    {"nombre": "Yoda", "episodios": [1, 2, 3, 5, 6, 8]},
-    {"nombre": "Boba Fett", "episodios": [2, 5, 6]},
-    {"nombre": "C-3PO", "episodios": [1, 2, 3, 4, 5, 6, 7, 8, 9]},
-    {"nombre": "Leia", "episodios": [4, 5, 6, 7, 8, 9]},
-    {"nombre": "Rey", "episodios": [7, 8, 9]},
-    {"nombre": "Kylo Ren", "episodios": [7, 8, 9]},
-    {"nombre": "Chewbacca", "episodios": [3, 4, 5, 6, 7, 8, 9]},
-    {"nombre": "Han Solo", "episodios": [4, 5, 6, 7]},
-    {"nombre": "R2-D2", "episodios": [1, 2, 3, 4, 5, 6, 7, 9]},
-    {"nombre": "BB-8", "episodios": [7, 8, 9]},
-]
+from personajes import personajes
 
 
 g_sw = Graph(is_directed=False)
@@ -81,4 +66,5 @@ print("personajes presentes en los 9 episodios:")
 for p in personajes:
     if len(p["episodios"]) == 9:
         print(p["nombre"])
+
 
